2bc649799d3819582a286c28a2bc1ce4add2a7ec
Nicole <nicole.yuqui@upb.edu.co>
2026-03-02 16:19:49 -0500

    configuración de plugin para vite de imagenes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      mozjpeg: { quality: 75 },
+      pngquant: { quality: [0.65, 0.8], speed: 4 },
+      svgo: {
+        plugins: [{ name: 'removeViewBox', active: false }],
+      },
+      gifsicle: { optimizationLevel: 7 },
+      webp: { quality: 75 },
+    }),
+  ],
   build: {
     minify: 'esbuild',
   },
